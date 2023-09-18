f7811f8ec3f233bf0de689863b438e148d47963d
vadose <81336230+urielduana@users.noreply.github.com>
2023-09-18 12:25:20 -0600

    Mongoose & Atlas config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 import express from "express";
 
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const port = env.PORT;
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Mongoose connected!");
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch(console.error);
