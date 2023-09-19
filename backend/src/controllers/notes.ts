3f69eeaf6aa78c3a9572cccb2286033b0f70d55d
vadose <81336230+urielduana@users.noreply.github.com>
2023-09-19 16:57:12 -0600

    Express HTTP error handling

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
   try {
@@ -14,18 +16,37 @@ export const getNote: RequestHandler = async (req, res, next) => {
   const id = req.params.id;
 
   try {
+    if (!mongoose.isValidObjectId(id)) {
+      throw createHttpError(400, "Invalid ID!");
+    }
     const note = await NoteModel.findById(id).exec();
+    if (!note) {
+      throw createHttpError(404, "Note not found!");
+    }
     res.status(200).json(note);
   } catch (error) {
     next(error);
   }
 };
 
-export const createNote: RequestHandler = async (req, res, next) => {
+interface CreateNoteBody {
+  title?: string;
+  text?: string;
+}
+
+export const createNote: RequestHandler<
+  unknown,
+  unknown,
+  CreateNoteBody,
+  unknown
+> = async (req, res, next) => {
   const title = req.body.title;
   const text = req.body.text;
 
   try {
+    if (!title) {
+      throw createHttpError(400, "Title is required!");
+    }
     const newNote = new NoteModel({ title, text });
     const note = await newNote.save();
     res.status(201).json(note);
