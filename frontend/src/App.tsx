2f070020a926d4e0d9ab02be557ce8ac63909eaa
vadose <81336230+urielduana@users.noreply.github.com>
2023-09-21 12:20:54 -0600

    READ frontend notes

---
+++ frontend/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import { Note as NoteModel } from "./models/note";
+import Note from "./components/Note";
+import { Col, Container, Row } from "react-bootstrap";
+import styles from "./styles/NotesPage.module.css";
+function App() {
+  const [notes, setNotes] = useState<NoteModel[]>([]);
+
+  useEffect(() => {
+    async function getNotes() {
+      try {
+        const response = await fetch("/api/notes", {
+          method: "GET",
+        });
+        const notes = await response.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    getNotes();
+  }, []);
+
+  return (
+    <Container>
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map((note) => (
+          <Col key={note._id}>
+            <Note note={note} className={styles.note} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
